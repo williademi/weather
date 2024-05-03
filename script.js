7bb4f0b7f914cc36dff6384468caab9594be8e0b
williademi <125095331+williademi@users.noreply.github.com>
2024-05-03 18:49:38 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('search-input');
+    const searchBtn = document.getElementById('search-btn');
+    const weatherInfo = document.getElementById('weather-info');
+    const API_KEY = 'ac1ad3fcb8093e69a64da62bcc49bd59' ;
+
+    
+})
